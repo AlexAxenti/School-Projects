4fe344d1d00b0f9cd18c9c40da0d212fa63e94f8
Alex Axenti <axentia@mcmaster.ca>
2020-02-12 01:29:33 +0000

    File Type Count feature comlete

--- Project1/project_analyze.sh
+++ Project1/project_analyze.sh
@@ -3,12 +3,11 @@
 # FIXME Log function
 # Called when arg1 = 1
 fixme_log() {
-	if [ ! -f fixme.log ] ; then
-		touch fixme.log
-	else
+	if [ -f fixme.log ] ; then
 		rm fixme.log
-		touch fixme.log
 	fi
+
+	touch fixme.log
 
 	for i in $(find . -name "*.txt") ; do
 		for j in $(tail -n 1 "$i") ; do
@@ -21,7 +20,25 @@ fixme_log() {
 
 }
 
+#File Type Count function
+#Called when arg2 = 2
+file_count() {
+	counter=0
+	read -p "Enter a file type: " filetype
+	for i in $(find . -name "*.$filetype") ; do
+		counter=$(($counter + 1))
+	done
+	echo $counter
+
+}
+
+
+#Script Input
+#Args 1-4 call a specified function
 if [ "$1" -eq 1 ] ; then
 	fixme_log
+elif [ "$1" -eq 2 ] ; then
+	file_count
 fi
+
 
