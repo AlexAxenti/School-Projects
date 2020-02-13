01fd01c9e30825992662a75010c439d62711d81d
Alex Axenti <axentia@mcmaster.ca>
2020-02-13 18:48:23 +0000

    File Tag Feature complete

---
+++ Project1/README.md
@@ -0,0 +1,22 @@
+# CS 1XA3 PRoject01 - axentia
+
+## Usage
+Execute this script from the project root with:
+	chmod +x CS1XA3/Project01/project_analyze.sh
+	./CS1XA3/Project1/project_analyze arg1
+There is only one required argument with possible inputs 1, 2, or 3.
+List of inputs with corresponding feature:
+1 - FIXME Log
+2 - File Type Count
+3 - Find Tag
+
+## Feature FIXME Log
+Description: This feature will find every file in your repo that contains the word '#FIXME' in its last line.
+These files will then be recorded into a new file (or overwritting the existing file) named fixme.log.
+
+Execution: To execute this file, arg1 when calling the script must be '1'
+	./CS1XA3/Project01/project_analyze 1
+
+Reference:
+How to find the last line of a file: https://kb.iu.edu/d/acrj 
+
--- Project1/project_analyze.sh
+++ Project1/project_analyze.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#todo check for white space and edge cases and more than one arguments
+
 # FIXME Log function
 # Called when arg1 = 1
 fixme_log() {
@@ -9,7 +11,7 @@ fixme_log() {
 
 	touch fixme.log
 
-	for i in $(find . -name "*.txt") ; do
+	for i in $(find .. -type f -name "*.txt") ; do
 		for j in $(tail -n 1 "$i") ; do
 			if [ "$j" = "#FIXME" ] ; then
 				echo "$i" >> fixme.log
@@ -17,28 +19,49 @@ fixme_log() {
 		done
 	done
 
-
 }
 
 #File Type Count function
-#Called when arg2 = 2
+#Called when arg1 = 2
 file_count() {
+
 	counter=0
 	read -p "Enter a file type: " filetype
-	for i in $(find . -name "*.$filetype") ; do
+	for i in $(find .. -name "*.$filetype") ; do
 		counter=$(($counter + 1))
 	done
 	echo $counter
 
 }
 
+#Find Tag feature
+#Called when arg1 = 3
+find_tag() {
+
+	read -p "Enter a tag to search for: " tag
+
+	if [ -f "$tag.log" ] ; then
+		rm "$tag.log"
+	fi
+	touch "$tag.log"
+	for i in $(find .. -name "*.py") ; do
+		IFS=$'\n'
+		for j in $(egrep "^#.*$tag.*" "$i") ; do
+			echo "$i" "$j" >> "$tag.log" 
+		done
+		#echo $(egrep "^#.*dog.*" "$i")
+	done
+
+}
 
 #Script Input
-#Args 1-4 call a specified function
+#Args 1-3 call a specified function
 if [ "$1" -eq 1 ] ; then
 	fixme_log
 elif [ "$1" -eq 2 ] ; then
 	file_count
+elif [ "$1" -eq 3 ] ; then
+	find_tag
 fi
 
 
