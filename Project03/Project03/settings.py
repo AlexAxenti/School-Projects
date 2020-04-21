a0199a8e47cc70d2dced05264a8103604b75f524
AlexAxenti <alex.axenti@gmail.com>
2020-04-21 16:42:29 -0400

    Update settings.py

--- Project03/Project03/settings.py
+++ Project03/Project03/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/u/macid/static/'
+STATIC_URL = '/u/axentia/static/'
 STATIC_ROOT = '/home/axentia/public_html/static/'
 STATICFILES_DIRS = [
                  os.path.join(BASE_DIR, "static"),
